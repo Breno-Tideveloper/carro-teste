1c39f987190995da64aa59eeced7c92fe3f096c6
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-12 16:00:52 -0300

    vamos bebeeeeeer

---
+++ Solucão Idade/Exercicio 1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Informe um nome: "); // writeline escreve informacão e quebra a linha depois.
+            var nome = Console.ReadLine(); // ReadLine le uma linha inteira com mais de 1 informaçao
+
+            Console.WriteLine("Informe a idade:");
+            int.TryParse(Console.ReadLine(), out int idade);//tryparse espera uma string e depois da virgula para separar
+                                                            //o os paramentros temos a saida do valor inserido.
+                                                            //colocando "out int" temos a criação da variavel
+                                                            //colocando apenas "out precisamos informar uma variavel existente.
+            if (idade > 18)
+            {
+                Console.WriteLine($"Partiu {nome}, beber uhull !!!!");
+
+            }
+            else
+            {
+                Console.WriteLine($"Infelizmente {nome}, voce não tem idade minima");
+            }
+
+            Console.ReadKey();
+           
+        }
+        
+
+
+    }
+}
